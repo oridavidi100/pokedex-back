e0c241a7c3dea26cef877a2f64b00fd5a5f91bcf
ori davidi <oridavidi100@gmail.com>
2021-10-22 12:42:55 +0300

    11

---
+++ src/index.js
@@ -0,0 +1,32 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const Pokedex = require('pokedex-promise-v2');
+const P = new Pokedex();
+
+app.use(express.static('public'));
+app.use(express.json()) // parses requests as json
+
+const pokemonRouter = require("./routers/pokemonRouter");
+const userRouter = require("./routers/userRouter");
+const {errorHandlerMiddleware} = require("./middleware/errorHandler")
+const {userHandler} = require("./middleware/userHandler");
+
+
+app.use(userHandler);
+
+app.use('/pokemon', pokemonRouter);
+app.use('/info', userRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(errorHandlerMiddleware);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
---
+++ src/routers/PokemonRouter.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const router = express.Router();
+const fs = require("fs");
+const path = require("path");
+
+
+const Pokedex = require('pokedex-promise-v2');
+const P = new Pokedex();
+
+//localhost:3000/pokemon 
+
+//Get pokemon collection
+router.get("/", (req, res)=> {
+    const userName = req.headers.username; //get user name
+    const usreFolderPath = path.resolve(`.\\users`, userName); //path to th user file
+    const pokemonCollection = [];
+    const collection = fs.readdirSync(usreFolderPath); //collection files from user folder
+    for (const pokemonFile of collection) {
+        const currentFilePath = path.resolve(usreFolderPath, pokemonFile); 
+        const fileContent = fs.readFileSync(currentFilePath).toString();
+        pokemonCollection.push(fileContent); //Array with pokemon collection
+    }
+    return res.send(pokemonCollection);                                          
+})
+
+
+//Pokemon information
+router.get("/get/:id", async (req, res)=> {
+        const id = req.params.id;
+        P.getPokemonByName(id)
+        .then((response) => {
+            const pokeObj = createPokeObj(response);
+            return res.send(pokeObj);
+        })
+        .catch((error)=> {
+          throw {"status": 404, "messege": "pokemon not found"};
+        });
+})
+
+router.get("/query", async (req, res)=> {
+    const pokename=req.body.query
+    P.getPokemonByName(pokename)
+    .then((response) => {
+        const pokeObj = createPokeObj(response);
+        return res.send(pokeObj);
+    })
+    .catch((error)=> {
+      throw {"status": 404, "messege": "pokemon not found"};
+    });
+})
+  
+       
+//Pokemon catch
+router.put("/catch/:id", (req, res)=> {
+    const id = req.params.id;
+    const userName = req.headers.username;
+    const usreFolderPath = path.resolve(`.\\users`, userName);
+    if(fs.existsSync(usreFolderPath)) {
+        const collection = fs.readdirSync(usreFolderPath); //all pokemon collection files
+        if (collection.includes(`${id}.json`)){ //Pokemon already caught
+            throw {"status": 403, "messege": "Pokemon already caught"};
+        }
+    } 
+    fs.writeFileSync(`${usreFolderPath}\\${id}.json`, `${JSON.stringify(req.body.pokemon)}`); //Add pokemon file with pokemon obj
+    return res.send(true);
+})
+
+//Delete pokemon from collection
+router.delete("/release/:id", (req, res)=> {
+    const id = req.params.id;
+    const userName = req.headers.username;
+    const usreFolderPath = path.resolve(`.\\users`, userName);
+    if(fs.existsSync(usreFolderPath)) {
+        const collection = fs.readdirSync(usreFolderPath);
+        if (collection.includes(`${id}.json`)){
+            fs.unlinkSync(`${usreFolderPath}\\${id}.json`); //Delete from collection
+            return res.send(true);
+        }
+        throw {"status": 403, "messege": "Pokemon is not in your collection"};//the user exists the pokemon doesn't
+    }
+})
+
+//create Poke Object from response
+function createPokeObj (pokeObj) {
+    const typesArray = [];
+    for(type of pokeObj.types) {
+        typesArray.push(type.type.name);
+    }
+    const abilitiesArray = [];
+    for(ability of pokeObj.abilities) {
+        abilitiesArray.push(ability.ability.name);
+    }
+    const pokedexObj = {
+        "name": pokeObj.forms[0].name, 
+        "height": pokeObj.height,
+        "weight": pokeObj.weight,
+        "types": typesArray,
+        "front_pic": pokeObj.sprites.front_default,
+        "back_pic": pokeObj.sprites.back_default,
+        "abilities": abilitiesArray
+    } 
+    return pokedexObj;
+}
+module.exports = router;
+
+
